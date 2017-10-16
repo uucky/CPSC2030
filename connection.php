20671f392257cc331d00ff6cd75be9158a01a657
uucky.lee@gmail.com <uucky.lee@gmail.com>
2017-10-16 01:34:11 -0700

    ...

--- connection.php
+++ connection.php
@@ -5,11 +5,9 @@ $password = "SkateBoard";
 $dbname = "pokedex";
 
 // Create connection
-function createConnection() {
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    } 
-}
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+} 
 ?>
